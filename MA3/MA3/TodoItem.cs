b30b4527b62926962fdae6b94972f53143c3e4d7
smurfgalway <g00323639@gmit.ie>
2017-12-11 20:50:26 +0000

    compiles

---
+++ MA3/MA3/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA3
+{
+
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string weight { get; set; }
+        public DateTimeOffset DOB { get; set; }
+        public string Height { get; set; }
+        public string age { get; set; }
+
+    }
+}
