56ad605579622d0e812e266a1173b02af454e9bf
smurfgalway <g00323639@gmit.ie>
2017-12-11 20:50:06 +0000

    working on serverside

--- MA3/MA3/CreatieProfile.xaml.cs
+++ MA3/MA3/CreatieProfile.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,14 +23,27 @@ namespace MA3
     /// </summary>
     public sealed partial class CreatieProfile : Page
     {
+        private IMobileServiceTable<TodoItem> table = App.MobileService.GetTable<TodoItem>();
+        private static MobileServiceCollection<TodoItem, TodoItem> mytable;
         public CreatieProfile()
         {
             this.InitializeComponent();
         }
 
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            TodoItem item = new TodoItem
+            {
+                Name = Name.Text,
+                UserName = UserName.Text,
+                Password = Password.Text,
+                //DOB = DOB.Date,
+            };
+            await table.InsertAsync(item);
+        }
+        public static MobileServiceCollection<TodoItem, TodoItem> getContent()
+        {
+            return mytable;
         }
     }
 }
