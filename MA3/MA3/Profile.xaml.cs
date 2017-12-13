2feb3b0c4a460ba29638924342a9dc6d50a49468
smurfgalway <g00323639@gmit.ie>
2017-12-13 19:51:45 +0000

    profile page added

---
+++ MA3/MA3/Profile.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MA3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile savedFile = await storageFolder.GetFileAsync("Profile.txt");
+
+
+            var stream = await savedFile.OpenAsync(Windows.Storage.FileAccessMode.Read); using
+                         (StreamReader reader = new StreamReader(stream.AsStream()))
+            {
+                textBlock.Text = reader.ReadToEnd();
+                textBlock.DataContext = reader.ReadToEnd();
+            }
+        }
+    }
+}
